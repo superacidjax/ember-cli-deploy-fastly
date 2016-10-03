7ed2d37627f700fc7cb684be48e1fb0ba0295257
Brian Dear <superacidjax@me.com>
2016-10-03 14:29:57 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,26 +1,78 @@
-# Ember-cli-deploy-fastly
+#Lightning Fast(ly) Deployment
 
-This README outlines the details of collaborating on this Ember addon.
+## Ember-cli-deploy-fastly
 
-## Installation
+NOTE: This is currently just a skeleton and doesn't actually do anything yet.
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+> An ember-cli-deploy plugin to upload index.html to a Redis store
 
-## Running
+This plugin uploads index.html to a [Fastly Edge Dictionary][1].
 
-* `ember server`
-* Visit your app at http://localhost:4200.
+This is based on a modified form of Ember Lightning Deployment first presented
+by James Rosen at [Fastly][2]
 
-## Running Tests
+This method requires custom VCL for Fastly and exploits Varnish synthetics.
 
-* `npm test` (Runs `ember try:testall` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
+You'll need to know about [Fastly VCL][3]. Assuming you know what you're doing,
+modify your ```sub vcl_recv``` to look like the following:
 
-## Building
+```
+sub vcl_recv {
+  unset req.http.X-App-Content;
+  if (req.url.path == "/" ||
+    req.url.path == "index.html") {
 
-* `ember build`
+    set req.http.X-App-Content =
+      table.lookup(appAssets,
+        "index.html");
 
-For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+    error 900 "index";
+  }
+
+
+#FASTLY recv
+
+  if (req.request != "HEAD" && req.request != "GET" && req.request != "FASTLYPURGE") {
+    return(pass);
+  }
+
+  return(lookup);
+}
+```
+
+This requires you to have an Edge Dictionary called ```appAssets``` -- you can
+change this to whatever you want. The name appAssets isn't critical, just be
+sure that the dictionary you call in the VCL has the same name as the dictionary (obviously!)
+
+You'll then need to modify your ```sub vcl_error``` to look like this:
+
+```
+sub vcl_error {
+  if (obj.status == 900) {
+    set obj.status = 200;
+    set obj.response = "OK";
+    synthetic req.http.X-App-Content;
+    return(deliver);
+  }
+
+#FASTLY error
+}
+```
+
+## How is this different than Lightning Deploy with Redis?
+
+Speed. You're essentially storing your index.html in Varnish while your assets
+live in S3. This means that your Ember app will probably be the fastest Ember app
+on the planet.
+
+# Contributors Wanted!
+
+I'm working on this on the side and there's a long ways to go before this is useable.
+Currently, I'm using ember-cli-deploy to get my assets to S3 and manually updating
+my edge dictionary via CURL. That's a horrible experience, hence the attempt at
+a plugin.
+
+
+[1]: https://docs.fastly.com/guides/edge-dictionaries/about-edge-dictionaries
+[2]: https://fastly.com
+[3]: https://docs.fastly.com/guides/vcl/mixing-and-matching-fastly-vcl-with-custom-vcl
--- index.js
+++ index.js
@@ -1,6 +1,173 @@
 /* jshint node: true */
 'use strict';
 
+var Promise   = require('ember-cli/lib/ext/promise');
+var path      = require('path');
+var fs        = require('fs');
+
+var denodeify = require('rsvp').denodeify;
+var readFile  = denodeify(fs.readFile);
+
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-fastly'
+  name: 'ember-cli-deploy-fastly',
+
+  createDeployPlugin: function(options) {
+    var Fastly = require('./lib/fastly');
+
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      defaultConfig: {
+        host: 'localhost',
+        filePattern: 'index.html',
+        distDir: function(context) {
+          return context.distDir;
+        },
+        keyPrefix: function(context){
+          return context.project.name() + ':index';
+        },
+        activationSuffix: 'current',
+        activeContentSuffix: 'current-content',
+        didDeployMessage: function(context){
+          var revisionKey = context.revisionData && context.revisionData.revisionKey;
+          var activatedRevisionKey = context.revisionData && context.revisionData.activatedRevisionKey;
+          if (revisionKey && !activatedRevisionKey) {
+            return "Deployed but did not activate revision " + revisionKey + ". "
+                 + "To activate, run: "
+                 + "ember deploy:activate " + context.deployTarget + " --revision=" + revisionKey + "\n";
+          }
+        },
+        revisionKey: function(context) {
+          return context.commandOptions.revision || (context.revisionData && context.revisionData.revisionKey);
+        },
+        fastlyDeployClient: function(context) {
+
+          ///
+          ///
+
+        },
+
+        revisionData: function(context) {
+          return context.revisionData;
+        }
+      },
+      configure: function(/* context */) {
+        this.log('validating config', { verbose: true });
+        ///
+
+        ///
+
+        ['filePattern', 'distDir', 'keyPrefix', 'activationSuffix', 'activeContentSuffix', 'revisionKey', 'didDeployMessage', 'redisDeployClient', 'maxRecentUploads', 'revisionData'].forEach(this.applyDefaultConfigProperty.bind(this));
+
+        this.log('config ok', { verbose: true });
+      },
+
+      upload: function(context) {
+        var fastlyDeployClient = this.readConfig('fastlyDeployClient');
+        var revisionKey       = this.readConfig('revisionKey');
+        var distDir           = this.readConfig('distDir');
+        var filePattern       = this.readConfig('filePattern');
+        var keyPrefix         = this.readConfig('keyPrefix');
+        var maxRecentUploads  = this.readConfig('maxRecentUploads');
+        var filePath          = path.join(distDir, filePattern);
+
+        this.log('Uploading `' + filePath + '`', { verbose: true });
+        return this._readFileContents(filePath)
+          .then(fastlyDeployClient.upload.bind(redisDeployClient, keyPrefix, revisionKey, this.readConfig('revisionData')))
+          .then(this._uploadSuccessMessage.bind(this))
+          .then(function(key) {
+            return { fastlyKey: key };
+          })
+          .catch(this._errorMessage.bind(this));
+      },
+
+      willActivate: function(/* context */) {
+        var fastlyDeployClient = this.readConfig('fastlyDeployClient');
+        var keyPrefix         = this.readConfig('keyPrefix');
+
+        var revisionKey = redisDeployClient.activeRevision(keyPrefix);
+
+        return {
+          revisionData: {
+            previousRevisionKey: revisionKey
+          }
+        };
+      },
+
+      activate: function(/* context */) {
+        var fastlyDeployClient   = this.readConfig('fastlyDeployClient');
+        var revisionKey         = this.readConfig('revisionKey');
+        var keyPrefix           = this.readConfig('keyPrefix');
+        var activationSuffix    = this.readConfig('activationSuffix');
+        var activeContentSuffix = this.readConfig('activeContentSuffix');
+
+        this.log('Activating revision `' + revisionKey + '`', { verbose: true });
+        return Promise.resolve(fastlyDeployClient.activate(keyPrefix, revisionKey, activationSuffix, activeContentSuffix))
+          .then(this.log.bind(this, '✔ Activated revision `' + revisionKey + '`', {}))
+          .then(function(){
+            return {
+              revisionData: {
+                activatedRevisionKey: revisionKey
+              }
+            };
+          })
+          .catch(this._errorMessage.bind(this));
+      },
+
+      didDeploy: function(/* context */){
+        var didDeployMessage = this.readConfig('didDeployMessage');
+        if (didDeployMessage) {
+          this.log(didDeployMessage);
+        }
+      },
+
+      fetchInitialRevisions: function(/* context */) {
+        var fastlyDeployClient = this.readConfig('fastlyDeployClient');
+        var keyPrefix = this.readConfig('keyPrefix');
+
+        this.log('Listing initial revisions for key: `' + keyPrefix + '`', { verbose: true });
+        return Promise.resolve(fastlyDeployClient.fetchRevisions(keyPrefix))
+          .then(function(revisions) {
+            return {
+              initialRevisions: revisions
+            };
+          })
+          .catch(this._errorMessage.bind(this));
+      },
+
+      fetchRevisions: function(/* context */) {
+        var fastlyDeployClient = this.readConfig('fastlyDeployClient');
+        var keyPrefix = this.readConfig('keyPrefix');
+
+        this.log('Listing revisions for key: `' + keyPrefix + '`');
+        return Promise.resolve(fastlyDeployClient.fetchRevisions(keyPrefix))
+          .then(function(revisions) {
+            return {
+              revisions: revisions
+            };
+          })
+          .catch(this._errorMessage.bind(this));
+      },
+
+      _readFileContents: function(path) {
+        return readFile(path)
+          .then(function(buffer) {
+            return buffer.toString();
+          });
+      },
+
+      _uploadSuccessMessage: function(key) {
+        this.log('Uploaded with key `' + key + '`', { verbose: true });
+        return Promise.resolve(key);
+      },
+
+      _errorMessage: function(error) {
+        this.log(error, { color: 'red' });
+        return Promise.reject(error);
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
